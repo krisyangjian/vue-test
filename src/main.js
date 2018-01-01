77624c6cab3569f643479e69383015362eeb8824
yangjian <yangjian@megvii.com>
2018-01-01 12:09:09 +0800

    sdsd

--- src/main.js
+++ src/main.js
@@ -1,24 +1,43 @@
-// import App from './app.vue'
+import App from './app.vue'
 // import Bpp from './b.vue'
-// import Vue from 'vue'
+import Vue from 'vue'
 
 
 // import './test.css'
-// import 'element-ui/lib/theme-chalk/index.css';
-// import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';
 
-// Vue.use(ElementUI);
-// Vue.component('app', App);
-// Vue.component('bpp', Bpp);
-// new Vue({
-//     el: '#app',
-//     template: '<div id="app"><app></app></div>',
-//     mounted: function() {
-//     },
-//     computed: {
-//     }
-//  });
-// import "babel-polyfill";
-const promise = new Promise(function(resolve, reject){});
-// console.log(Object.assign({}, {'name': 'yangjian'}))
-// new Promise()
+import enLocale from 'element-ui/lib/locale/lang/en'
+import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
+
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n)
+
+
+const messages = {
+  'en': enLocale,
+  'zh-CN': zhLocale
+}
+
+// console.log(megs)
+const i18n = new VueI18n({
+  locale: 'zh-CN',
+  messages
+})
+Vue.use(ElementUI, {
+  i18n: function(key,value) {
+    return 'ss';
+  }
+});
+new Vue({
+    el: '#app',
+    i18n,
+    template: '<div id="app"></div>',
+    mounted() {
+      this.$confirm("确认删除", "确认", function(){}, function(){});
+      setTimeout(() => {
+        this.$i18n.locale = "en";
+      })
+    }
+ });
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,22 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 const SimpleProgressWebpackPlugin = require( 'simple-progress-webpack-plugin' );
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 
 module.exports = {
   entry: {
+    // vendor: 'vue',
     app: './src/main.js'
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'app.js',
+    filename: '[name].js'
+    // publicPath: '/vue-test/dist'
   },
+  // externals: {
+  //   vue: 'vue'
+  // },
   module: {
     rules: [
       {
@@ -23,7 +30,7 @@ module.exports = {
       {
         test: /\.js$/,
         include: path.resolve(__dirname, "src"),
-        use: ['babel-loader']
+        use: 'babel-loader'
       },
       {
         test: /\.css$/,
@@ -38,8 +45,8 @@ module.exports = {
   devServer: {
     // quiet: true,
     noInfo: true,
-    port: '9000'
-    // hot: true
+    port: '9000',
+    hot: true
     // contentBase: './dist'
   },
   plugins: [
@@ -47,14 +54,31 @@ module.exports = {
     new HTMLWebpackPlugin({
       template: path.join(__dirname, 'index.html'),
     }),
-    // new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     // new SimpleProgressWebpackPlugin({
     //   format: 'expanded'
     // }),
+    // new LodashModuleReplacementPlugi(),
     new FriendlyErrorsPlugin()
+    // new webpack.optimize.CommonsChunkPlugin({
+      // name: 'vendor'
+    //   minChunks: Infinity
+    // })
+    // new CompressionWebpackPlugin({
+    //   asset: '[path].gz[query]',
+    //   algorithm: 'gzip',
+    //   test: new RegExp(
+    //     '\\.(' +
+    //     ['js', 'css'].join('|') +
+    //     ')$'
+    //   ),
+    //   threshold: 10240,
+    //   minRatio: 0.8
+    // })
   ],
   resolve: {
     alias: {
+      'aaa': path.resolve(__dirname, './'),
       'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
     }
   }
